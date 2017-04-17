a183f37801071b609f12fe9bbdecde81198a961e
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-16 23:43:28 -0300

    Initial scaffolding with unit tests

--- src/components/Recordings.js
+++ src/components/Recordings.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
+import Recording from './Recording';
 
-const Recordings = () => {
-  return <div>Recordings Page</div>;
+class Recordings extends Component {
+  render() {
+    let content = null;
+
+    // if we have recordings, render them
+    if (this.props.recordings && this.props.recordings.length > 0) {
+      content = _.map(this.props.recordings, (fieldProps) => <Recording {...fieldProps} key={fieldProps.url} />);
+    }
+    // else render a loading indicator
+    else {
+      content = <CircularProgress />;
+    }
+
+    return (
+      <div className='recordings-wrapper'>
+        {content}
+      </div>
+    );
+  }
 };
 
 export default Recordings;
---
+++ src/components/Recordings.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Route } from 'react-router';
+import { shallow } from 'enzyme';
+import CircularProgress from 'material-ui/CircularProgress';
+import Recordings from './Recordings';
+import Recording from './Recording';
+
+describe('<Recordings />', () => {
+  it('shows a spinner while requesting for data', () => {
+    const wrapper = shallow(<Recordings />);
+    const spinner = wrapper.find(CircularProgress);
+
+    expect(spinner).toHaveLength(1);
+  });
+
+  it('shows a list of <Recording /> elements', () => {
+    const recording = {
+      "final_script": "transcript text",
+      "rating": 4,
+      "duration": 920,
+      "url": "url_to_audio", 
+      "created": "date_string" 
+    };
+    const props = {
+      recordings: [
+        recording,
+        recording,
+        recording,
+        recording
+      ]
+    }
+
+    const wrapper = shallow(<Recordings {...props} />);
+    const recordings = wrapper.find(Recording);
+    expect(recordings).toHaveLength(4);
+  });
+
+  it('shows a Logout button', () => {
+
+  });
+
+});
