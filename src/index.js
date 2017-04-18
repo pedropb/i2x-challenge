dc429e1c6eda329bff935220ff45442586bbb994
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-17 22:14:45 -0300

    Material-UI theme customization

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-//import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-//import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { indigo500 } from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -12,12 +12,18 @@ import reducers from './reducers';
 
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: indigo500
+  }
+});
+
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(reducers, undefined, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
       <Routes />
     </MuiThemeProvider>
   </Provider>,
