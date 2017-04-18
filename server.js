706414bb6d4fac11ae139f31731bd6f2b1e15297
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-18 03:50:11 -0300

    Tuning server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static('./build'));
+app.use(express.static(path.join(__dirname, './build')));
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(80);
-console.log('Server started on port 80');
+app.listen(port);
+console.log(`Server started on port ${port}`);
